3f616e4b26db6018639bb72cb60ffc48f3c7412e
mrtim <charly.simon.cs@gmail.com>
2016-10-16 20:30:09 -0400

    fix No adjusted value for ibfeed

--- tutorial-4.py
+++ tutorial-4.py
@@ -13,7 +13,7 @@ class MyStrategy(strategy.BacktestingStrategy):
         self.__position = None
         self.__instrument = instrument
         # We'll use adjusted close values instead of regular close values.
-        self.setUseAdjustedValues(True)
+        #self.setUseAdjustedValues(True)
         self.__sma = ma.SMA(feed[instrument].getPriceDataSeries(), smaPeriod)
 
     def onEnterOk(self, position):
