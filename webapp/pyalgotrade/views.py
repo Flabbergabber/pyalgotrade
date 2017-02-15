c822c982037cde4a39b4c4783d6fe127ae9f3446
David Méthot <methot.david@gmail.com>
2017-02-15 15:56:51 -0500

    Ajout d'un proof of concept pour l'execution de code Python in-browser.

--- webapp/pyalgotrade/views.py
+++ webapp/pyalgotrade/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
+import json
+from django.http import Http404, HttpResponse
+import sys
+import StringIO
+import contextlib
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'pyalgotrade/index.html')
+
+
+def testExec(request):
+    return render(request, 'pyalgotrade/testExec.html')
+
+
+@contextlib.contextmanager
+def stdoutIO(stdout=None):
+    old = sys.stdout
+    if stdout is None:
+        stdout = StringIO.StringIO()
+    sys.stdout = stdout
+    yield stdout
+    sys.stdout = old
+
+def execStrategy(request):
+    if request.is_ajax() and request.POST:
+        code = request.POST.get('strategy')
+        with stdoutIO() as s:
+            exec(code)
+
+        execResult = s.getvalue()
+        data = {'message': execResult}
+        return HttpResponse(json.dumps(data), content_type='application/json')
+    else:
+        raise Http404
